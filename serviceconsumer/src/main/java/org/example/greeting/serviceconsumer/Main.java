24a425e798fca8b5cb1780d0841ccd9c934bde35
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-04-30 13:40:08 +0200

    Implement main class with language selection and greeting via SPI

--- serviceconsumer/src/main/java/org/example/greeting/serviceconsumer/Main.java
+++ serviceconsumer/src/main/java/org/example/greeting/serviceconsumer/Main.java
@@ -1,4 +1,41 @@
 package org.example.greeting.serviceconsumer;
 
+import org.example.greeting.service.GreetingLanguage;
+import org.example.greeting.service.GreetingService;
+
+import java.lang.annotation.Annotation;
+import java.util.*;
+
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ServiceLoader<GreetingService> greetings = ServiceLoader.load(GreetingService.class);
+        Map<Integer, GreetingService> choice = new HashMap<>();
+
+        System.out.println("Choice your language: ");
+        System.out.println(" ");
+
+        int i = 1;
+        for (GreetingService service : greetings) {
+            GreetingLanguage language = service.getClass().getAnnotation(GreetingLanguage.class);
+            if (language != null) {
+                System.out.println(i + ": "  + language.value());
+                choice.put(i, service);
+                i++;
+            } else {
+                System.out.println("ERROR: No GreetingLanguage annotation found" + service.getClass().getName());
+            }
+        }
+        System.out.println(" ");
+        int languageChoice = Integer.parseInt(sc.nextLine());
+
+        GreetingService select = choice.get(languageChoice);
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
+
+        System.out.println(select.greetingTranslate(name));
+    }
 }
