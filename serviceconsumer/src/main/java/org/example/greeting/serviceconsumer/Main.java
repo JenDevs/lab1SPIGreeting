fc88fc5e5bc6647914a79cbe33a11e7d9d6cbdb4
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-04-30 16:30:43 +0200

    Minor changes to Main method structure

--- serviceconsumer/src/main/java/org/example/greeting/serviceconsumer/Main.java
+++ serviceconsumer/src/main/java/org/example/greeting/serviceconsumer/Main.java
@@ -9,29 +9,28 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        ServiceLoader<GreetingService> greetings = ServiceLoader.load(GreetingService.class);
+        List<GreetingService> implementations = new ArrayList<>();
+        ServiceLoader.load(GreetingService.class).forEach(implementations::add);
         Map<Integer, GreetingService> choice = new HashMap<>();
 
-        System.out.println("Choice your language: ");
-        System.out.println(" ");
+        System.out.println("Choice your language:");
 
         int i = 1;
-        for (GreetingService service : greetings) {
+        for (GreetingService service : implementations) {
             GreetingLanguage language = service.getClass().getAnnotation(GreetingLanguage.class);
             if (language != null) {
-                System.out.println(i + ": "  + language.value());
+                System.out.println(i + ": " + language.value());
                 choice.put(i, service);
                 i++;
             } else {
-                System.out.println("ERROR: No GreetingLanguage annotation found" + service.getClass().getName());
+                System.out.println("ERROR: No GreetingLanguage annotation found: " + service.getClass().getName());
             }
         }
-        System.out.println(" ");
-        int languageChoice = Integer.parseInt(sc.nextLine());
 
+        int languageChoice = Integer.parseInt(sc.nextLine());
         GreetingService select = choice.get(languageChoice);
+
         System.out.println("What is your name?");
         String name = sc.nextLine();
 
